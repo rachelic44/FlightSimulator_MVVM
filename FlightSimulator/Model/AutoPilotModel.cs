d87637a8283a5bb94fc3c2eadfb7da49af8a0c80
yael91 <shanyyael.dagan@live.biu.ac.il>
2019-04-22 16:26:31 +0300

    AutoPilotViewModel clear

---
+++ FlightSimulator/Model/AutoPilotModel.cs
@@ -0,0 +1,15 @@
+﻿using FlightSimulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightSimulator.Model
+{
+    public class AutoPilotModel : BaseNotify
+    {
+
+
+    }
+}
